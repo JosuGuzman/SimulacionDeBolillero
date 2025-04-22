bc0530d08454b5c5f8b5fd8a1c3a3f687eb12473
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-04-22 00:16:37 -0300

    Clase Bolillero y Solucion "Simulador Bolillero"

---
+++ Consola/Bolillero.cs
@@ -0,0 +1,67 @@
+namespace Consola;
+
+public class Bolillero
+{
+    private readonly ISorteadorRandom _sorteador;
+    private readonly List<int> _bolillasInside;
+    private readonly List<int> _bolillasOutside = new();
+
+    public IReadOnlyList<int> BolillasInside => _bolillasInside;
+    public IReadOnlyList<int> BolillasOutside => _bolillasOutside;
+
+    public Bolillero(IEnumerable<int> bolillasIniciales, ISorteadorRandom sorteador)
+    {
+        _bolillasInside = new List<int>(bolillasIniciales);
+        _sorteador = sorteador;
+    }
+    public int SacarBolilla()
+    {
+        if (_bolillasInside.Count == 0)
+            throw new InvalidOperationException("No hay más bolillas para sacar.");
+        
+        int idx = _sorteador.Next(0, _bolillasInside.Count);
+        
+        int valor = _bolillasInside[idx];
+        
+        _bolillasInside.RemoveAt(idx);
+        
+        _bolillasOutside.Add(valor);
+        return valor;
+    }
+
+    public void ReIngresar()
+    {
+        _bolillasInside.AddRange(_bolillasOutside);
+        _bolillasOutside.Clear();
+    }
+
+    public bool Jugar(IList<int> jugada)
+    {
+        foreach (var objetivo in jugada)
+        {
+            int sacada = SacarBolilla();
+            if (sacada != objetivo)
+            {
+                ReIngresar();
+                return false;
+            }
+        }
+
+        ReIngresar();
+
+        return true;
+    }
+
+    
+    public long JugarNVeces(IList<int> jugada, long veces)
+    {
+        long aciertos = 0;
+        
+        for (long i = 0; i < veces; i++)
+            
+            if (Jugar(jugada))    
+                aciertos++;
+        
+        return aciertos;
+    }
+}
