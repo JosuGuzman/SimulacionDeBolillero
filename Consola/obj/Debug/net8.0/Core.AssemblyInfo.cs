dc10a54e966b2326e2a509480cc084c577fe8130
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-05-13 16:57:06 -0300

    a

--- Consola/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Consola/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b744b72e878c97702818fe4faa9f83d76c9f91e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35d44e58a80806ea63d50c232d7ca341c8492977")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
