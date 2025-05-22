bc4864bf0e233478fab88560646a5c59cb8baddb
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-05-22 14:07:16 -0300

    no impoera

--- TestsBolillero/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ TestsBolillero/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc10a54e966b2326e2a509480cc084c577fe8130")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a65cbc2dbfbd0772ff0bd11606a6acb25c50f4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
